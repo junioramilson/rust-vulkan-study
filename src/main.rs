230515fc9166f64d36aa983d66e0bf5e9c8abfcd
Amilson Junior <amilson_junior@outlook.com>
2023-02-04 04:24:24 -0300

    remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,11 @@
-use image::{ImageBuffer, Rgba};
 use image_examples::create_simple_image::{generate_simple_image, ImageSizeInfo};
-use vulkano::sync::GpuFuture;
-use vulkano::{VulkanLibrary, sync};
-use vulkano::buffer::{CpuAccessibleBuffer, BufferUsage};
+use vulkano::{VulkanLibrary};
 use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
-use vulkano::command_buffer::{AutoCommandBufferBuilder, CommandBufferUsage, ClearColorImageInfo, CopyImageToBufferInfo};
 use vulkano::device::{DeviceCreateInfo, QueueCreateInfo, Device};
-use vulkano::format::Format;
-use vulkano::image::{StorageImage, ImageDimensions};
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::memory::allocator::{StandardMemoryAllocator, GenericMemoryAllocator};
-use vulkano::pipeline::graphics;
+use vulkano::memory::allocator::{StandardMemoryAllocator};
 
 mod image_examples;
-
-use bytemuck::{Pod, Zeroable};
-
-/*
-    https://vulkano.rs/guide
-*/
-
-#[repr(C)]
-#[derive(Default, Copy, Clone, Zeroable,  Pod)]
-struct MyStruct {
-    a: u32,
-    b: u32
-}
 
 fn main() {
     let library = VulkanLibrary::new().expect("Local Vulkan lib not found");
@@ -55,7 +35,8 @@ fn main() {
         },
     ).expect("Failed to create device");
 
-    let queue = queues.next().unwrap(); // Now ready to ask the GPU to perform operations
+    let queue = queues.next().unwrap();
+
     let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
     let command_buffer_allocator =
         StandardCommandBufferAllocator::new(device.clone(), Default::default());
